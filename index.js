8e6be829fe8cd200b76763129f74f6e103abde86
unknown <opokudavid446@gmail.com>
2020-08-12 02:24:39 +0100

    :wrench:

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const productRoutes = require("./routes/productsRoutes");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use("/public", express.static("public"));
 
